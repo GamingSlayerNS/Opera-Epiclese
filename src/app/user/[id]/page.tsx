82511efd91c7630d06a923a6f58e69b2c8f7ca06
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-11-24 16:55:50 -0600

    Update page.tsx

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -4,23 +4,22 @@ import { EnkaClient } from 'enka-network-api';
 // Change the directory to store cache data.
 // Default directory is node_modules/enka-network-api/cache.
 const enka = new EnkaClient({ cacheDirectory: "./cache", showFetchCacheLog: false, defaultLanguage: "en" });
+enka.cachedAssetsManager.cacheDirectorySetup();
+enka.cachedAssetsManager.fetchAllContents();
 
-const Player = ({ params }: any) => {
-    enka.cachedAssetsManager.cacheDirectorySetup();
-    enka.cachedAssetsManager.fetchAllContents();
+// const characters = enka.getAllCharacters();
 
-    const characters = enka.getAllCharacters();
-
-    const renderCharaList = (): any => {
-        return characters.map((c: any, i) => {
-            return <p key={i}>{c.name.get()}</p>
-        })
-    }
+const Player = async ({ params }: any) => {
+    // const renderCharaList = (): any => {
+    //     return characters.map((c: any, i) => {
+    //         return <p key={i}>{c.name.get()}</p>
+    //     })
+    // }
 
     return (
         <div>
             <div>{params.id}</div>
-            {renderCharaList()}
+            {/* {renderCharaList()} */}
         </div>
     )
 }
