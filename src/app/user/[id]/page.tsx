52a7aed4e691e070d09751f45eb51dd703b5121f
GamingSlayerNS <naxelalejandro@hotmail.com>
2023-11-24 01:53:11 -0600

    Added Components

---
+++ src/app/user/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Player = ({ params }: any) => {
+    return (
+        <div>{params.id}</div>
+    )
+}
+
+export default Player;
---
+++ src/components/searchBtn.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const SearchBtn = () => {
+  return (
+    <div>
+        <button onClick={() => {}}>Click</button>
+    </div>
+  )
+}
+
+export default SearchBtn;
